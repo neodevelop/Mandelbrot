fd21e482e856d5004452754c9a6cfb3acb510302
neodevelop <neodevelop@gmail.com>
2011-11-04 11:55:36 -0600

    Initial Commit

---
+++ Mandelbrot/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Mandelbrot
+//
+//  Created by neodevelop on 04/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Mandelbrot/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Mandelbrot
+//
+//  Created by neodevelop on 04/11/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
